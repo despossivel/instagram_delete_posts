294089a6b1a1da0196e867178214ed7fd1065a79
Matheus Brito <mattbmoller@gmail.com>
2021-02-17 11:22:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const Instagram = require('instagram-web-api')
-const FileCookieStore = require('tough-cookie-filestore2')
-const cookieStore = new FileCookieStore('./cookies.json')
-const client = new Instagram({ username: 'depoisdomeme', password: 'Spud@123.', cookieStore })
+const Instagram = require('instagram-web-api'),
+      FileCookieStore = require('tough-cookie-filestore2'),
+      cookieStore = new FileCookieStore('./cookies.json'),
+      username = 'desposdomeme',
+      client = new Instagram({ username, password: '', cookieStore })
 
     ; (async () => {
         await client.login()
@@ -10,11 +11,12 @@ const client = new Instagram({ username: 'depoisdomeme', password: 'Spud@123.', 
     })()
 
 const runDelete = async () => {
-    const { user: { edge_owner_to_timeline_media: { edges } } } = await client.getPhotosByUsername({ username: 'depoisdomeme', first: 100 })
-    // photos.user.edge_owner_to_timeline_media.
+    const { user: { edge_owner_to_timeline_media: { edges } } } = await client.getPhotosByUsername({ username, first: 100 })
+
     edges.map(async ({ node: { id } }, i) => {
         const deleteResponse = await client.deleteMedia({ mediaId: id })
 
+        
         if (deleteResponse.did_delete) {
             console.log(`${id} deletado!`)
         }
